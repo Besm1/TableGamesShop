cec9d6aa61d5ec215b1d7d5bd6bd5962cd9f2653
b.smirnov <besm1@yandex.ru>
2024-11-12 21:26:17 +0300

    module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -4,18 +4,31 @@ import sqlite3
 connection = sqlite3.Connection("database.db")
 cursor = connection.cursor()
 
-cursor.execute('drop table if exists users')
-connection.commit()
+# cursor.execute('drop table if exists users')
+# connection.commit()
+#
+cursor.execute('''
+create table if not exists Users (
+id integer primary key 
+, username text NOT NULL
+, email not null
+, age integer
+, balance integer)
+''')
 
-cursor.execute('create table if not exists Users (id integer primary key ,username text NOT NULL,email not null,age int, balance integer)')
+for i in range(1, 11):
+    cursor.execute('insert into Users (username, email, age, balance) values (?, ?, ?, ?)',
+                   (f'newuser{i}', f'example{i}@gmail.com', 10*i, 1000))
 
-cursor.execute("create index if not exists idx_email on Users (email)")
-
-# cursor.execute('insert into Users (username, email, age) values (?, ?, ?)', ('newuser', 'ex@gmail.com', 28))
-for i in range(30):
-    cursor.execute('insert into Users (username, email, age) values (?, ?, ?)', (f'newuser{i}', f'ex{i}@gmail.com', random.randint(20,60)))
-
-cursor.execute('delete from Users where username = ?', ('newuser',))
+cursor.execute('update users set balance = 500 where id % 2 = 1')
+cursor.execute('delete from users where (id - 1) % 3 = 0')
+cursor.execute('select username, email, age, balance from Users')
+recs = cursor.fetchall()
+f_names =  ['Имя', 'Почта', 'Возраст', 'Баланс']
+for rec_ in recs:
+    for i in range(4):
+        print(f'{f_names[i]}: {rec_[i]}', end='\t| ')
+    print()
 
 connection.commit()
 connection .close()
