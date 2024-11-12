ef5bd4b41888aa564bd7b4bde787d57d6ea6dba5
b.smirnov <besm1@yandex.ru>
2024-11-12 22:09:43 +0300

    module_14_1.py

--- db.py
+++ db.py
@@ -4,18 +4,23 @@ import sqlite3
 connection = sqlite3.Connection("database.db")
 cursor = connection.cursor()
 
-cursor.execute('drop table if exists users')
+# cursor.execute('drop table if exists Users_Test')
+# connection.commit()
+#
+# cursor.execute('create table if not exists Users_Test (id integer primary key ,username text NOT NULL,email not null,age int)')
+#
+# cursor.execute("create index if not exists idx_email on Users_Test (email)")
+#
+# # cursor.execute('insert into Users_Test (username, email, age) values (?, ?, ?)', ('newuser', 'ex@gmail.com', 28))
+# for i in range(30):
+#     cursor.execute('insert into Users_Test (username, email, age) values (?, ?, ?)', (f'newuser{i}', f'ex{i}@gmail.com', random.randint(20,60)))
+#
+# cursor.execute('delete from Users_Test where username = ?', ('newuser',))
+
+cursor.execute('select max(age) from users')
+print(cursor.fetchall(), cursor.fetchall())
+
+
+
 connection.commit()
-
-cursor.execute('create table if not exists Users (id integer primary key ,username text NOT NULL,email not null,age int)')
-
-cursor.execute("create index if not exists idx_email on Users (email)")
-
-# cursor.execute('insert into Users (username, email, age) values (?, ?, ?)', ('newuser', 'ex@gmail.com', 28))
-for i in range(30):
-    cursor.execute('insert into Users (username, email, age) values (?, ?, ?)', (f'newuser{i}', f'ex{i}@gmail.com', random.randint(20,60)))
-
-cursor.execute('delete from Users where username = ?', ('newuser',))
-
-connection.commit()
-connection .close()
+connection.close()
--- module_14_1.py
+++ module_14_1.py
@@ -1,15 +1,15 @@
 import random
 import sqlite3
 
-connection = sqlite3.Connection("database.db")
+connection = sqlite3.Connection("not_telegram.db")
 cursor = connection.cursor()
 
-# cursor.execute('drop table if exists users')
-# connection.commit()
-#
+cursor.execute('drop table if exists users')
+connection.commit()
+
 cursor.execute('''
 create table if not exists Users (
-id integer primary key 
+id integer primary key
 , username text NOT NULL
 , email not null
 , age integer
@@ -22,13 +22,13 @@ for i in range(1, 11):
 
 cursor.execute('update users set balance = 500 where id % 2 = 1')
 cursor.execute('delete from users where (id - 1) % 3 = 0')
-cursor.execute('select username, email, age, balance from Users')
+cursor.execute('select username, email, age, balance from Users where age <> 60')
 recs = cursor.fetchall()
 f_names =  ['Имя', 'Почта', 'Возраст', 'Баланс']
 for rec_ in recs:
     for i in range(4):
         print(f'{f_names[i]}: {rec_[i]}', end='\t| ')
-    print()
+    print(rec_)
 
 connection.commit()
 connection .close()
